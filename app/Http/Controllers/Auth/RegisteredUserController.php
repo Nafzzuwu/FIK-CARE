139e3e8179b15f604963e1500cc3adb1a9f9abdb
Nafariel 75 <nafarielkom@gmail.com>
2025-11-27 17:46:37 +0700

    updated

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,8 +44,7 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
-        return redirect(route('dashboarduser', absolute: false));
+        return redirect()->route('login')
+        ->with('success', 'Registrasi berhasil! Silakan login.');
     }
 }
